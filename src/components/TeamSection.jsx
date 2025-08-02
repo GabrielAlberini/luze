6bb213cde6ef9495115eeca10d9fd3708d7b9784
gabrielalberini <gabialberini733@gmail.com>
2025-08-02 16:57:36 -0300

    change: image hero section

--- src/components/TeamSection.jsx
+++ src/components/TeamSection.jsx
@@ -25,7 +25,7 @@ const TeamSection = () => {
               <h3>Gabriel Alberini</h3>
               <p className="team-role">Producción y Fabricación</p>
               <p className="team-description">
-                Soy Gabriel, responsable del proceso de elaboración de las alfombras. Algunas de mis tareas incluyen trasladar el diseño al bastidor, realizar el tufteado, recortar la alfombra del lienzo y aplicar la base de tela que se adhiere al piso.
+                Gabriel es el responsable del proceso de elaboración de las alfombras. Entre sus tareas se encuentran trasladar el diseño al bastidor, realizar el tufteado, recortar la alfombra del lienzo y aplicar la base de tela que se adhiere al piso.
               </p>
             </div>
           </div>
@@ -44,7 +44,7 @@ const TeamSection = () => {
               <h3>Luciana Giné</h3>
               <p className="team-role">Gestión y Contenido</p>
               <p className="team-description">
-                Luciana se encarga de la gestión del taller, controlando el devanado de lana, el stock y el estado de los materiales. Además, es la responsable de la comunicación del emprendimiento, creando y gestionando el contenido para nuestras redes sociales, especialmente en Instagram.
+                Luciana se encarga de la gestión del taller, controlando el ovillado de lana, el stock y el estado de los materiales. Además, es la responsable de la comunicación del emprendimiento, creando y gestionando el contenido para nuestras redes sociales, especialmente en Instagram.
               </p>
             </div>
           </div>
